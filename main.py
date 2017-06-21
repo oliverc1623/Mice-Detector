03f0f6c662e67f613663a2f75ee9e4c023bc0aeb
ollieskywalker <ollieskywalker@gmail.com>
2017-06-21 15:25:37 -0700

    Working on ubuntu

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import usb
-
 from keyboard_alike import reader
 
-readerCurr = readerCurr.Reader(0x16C0, 0x27DB, 84, 16, should_reset=True)
-
-readerCurr.initialize()
-print(readerCurr.read().strip())
-                       
-readerCurr.disconnect()
+reader = reader.Reader(0x16c0,  0x27db, 36, 16, should_reset=False)
+reader.initialize()
+while True:
+	print(reader.read().strip())
+reader.disconnect()
