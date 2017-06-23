74b3f0c8ba8545599acd6b6a7250f905a42f92c7
ollieskywalker <ollieskywalker@gmail.com>
2017-06-23 14:31:37 -0700

    Code reads id code

--- main.py
+++ main.py
@@ -1,63 +1,11 @@
 from keyboard_alike import reader
-from PyQt4 import QtCore, QtGui
-import sys
-from pyqt_ui import Ui_MainWindow
 
-class ReadFromUSB(QtCore.QThread):
-    def __init__(self, parent):
-        super(ReadFromUSB, self).__init__(parent)
-        self.parent = parent
+#by Oliver Chang 6/23/17
 
-    def run(self):
-        try:
-            self.parent.usb_read_value = self.parent.reader.read().strip()
-        except (reader.DeviceException, reader.ReadException) as e:
-            self.parent.usb_exception = str(e)
-            self.parent.usb_exception_counter += 1
+reader1 = reader.Reader(0x16c0,  0x27db, 36, 3, should_reset=False)
+'''reader2 = reader.Reader(0x16c0,  0x27db, 36, 3, should_reset=False)'''
 
-class StartQT4(QtGui.QMainWindow):
-    def __init__(self, parent=None):
-        QtGui.QWidget.__init__(self, parent)
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-        self._set_initial_usb_values()
-
-        self.reader = reader.Reader(0x16c0, 0x27db, 36, 16, should_reset=False)
-        self.reader.initialize()
-
-        self._start_reader_thread()
-	
-    def _set_initial_usb_values(self):
-        self.usb_read_value = None
-        self.usb_exception = None
-        self.usb_exception_counter = 1
-
-    def _start_reader_thread(self):
-        thread = ReadFromUSB(self)
-        thread.start()
-        thread.finished.connect(self._receive_data)
-
-    def _receive_data(self):
-        if not self.usb_read_value:
-            print('* Ups, no data')
-        else:
-            print(self.usb_read_value)
-            self.ui.dataList.addItem(self.usb_read_value)
-            self._set_initial_usb_values()
-
-        if self.usb_exception_counter >= 3:
-            print(self.usb_exception)
-            self.rfidreader.disconnect()
-            QtGui.QApplication.quit()
-        else:
-            if self.usb_exception:
-                print(self.usb_exception)
-                self.usb_exception = None
-            self._start_reader_thread()
-
-if __name__ == "__main__":
-    app = QtGui.QApplication(sys.argv)
-    myapp = StartQT4()
-    myapp.show()
-    sys.exit(app.exec_())
-
+reader1.initialize()
+while True:
+	print('The current rfid code is...... ' + str(reader1.read()))
+reader1.disconnect()
